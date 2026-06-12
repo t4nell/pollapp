f09b5fe2dd656adf0d85a0a5511144ec2ddb8b42
Michel Korfmacher <michelkorfmacher@yahoo.de>
2026-06-12 18:05:01 +0200

    feat: add YourSurveys component and integrate it into the app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { StartComponent } from "./main_page/components/landing_page/startComponent.component";
+import { YourSurveys } from "./main_page/components/your_surveys/yourSurveys.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, StartComponent],
+  imports: [RouterOutlet, HeaderComponent, StartComponent, YourSurveys],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/main_page/components/your_surveys/yourSurveys.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'your-surveys',
+  standalone: true,
+  templateUrl: './yourSurveys.component.html',
+  styleUrl: './yourSurveys.component.scss',
+})
+
+  
+
+export class YourSurveys {}
